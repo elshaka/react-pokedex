2f2eb79afc5d310411caa73f62c057ca47923140
Eleazar Meza <bairesdev_emeza@iseatz.com>
2020-04-10 17:55:10 -0400

    Created redux store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+const rootReducer = (state, { action, payload }) => {
+  switch (action) {
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
